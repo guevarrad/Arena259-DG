060d81c351012388b942209eca655b6bc5b8f8c6
grumpywizardstudent <klein@sou.edu>
2026-04-22 16:58:17 -0700

    Adding a class for randomizing values across the project.

---
+++ src/arenarand.cpp
@@ -0,0 +1,6 @@
+#include "willynilly.h"
+
+
+bool ArenaRand::flipCoin() { return COIN(gen); }
+
+int ArenaRand::randomValue(int min, int max) { return std::uniform_int_distribution<>{min, max}(gen); }
--- src/random.cpp
+++
@@ -1,5 +0,0 @@
-#include "random.h"
-
-bool Random::flipCoin() { return COIN(yield); }
-
-int Random::randomValue(int min, int max) { return std::uniform_int_distribution<>{min, max}(yield); }
